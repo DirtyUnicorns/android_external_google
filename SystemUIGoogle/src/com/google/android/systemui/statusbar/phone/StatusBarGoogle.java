d24532fb05a75dc42204dea6ee54c20404662fe1
Nicolas Dhouailly <ndhouailly@gmail.com>
2019-12-31 21:05:48 +0100

    Enable Motion Sense plugin by default
    Change-Id: I591bd4f8b5a52143eb30fbde06df6655b06ebc9d

--- SystemUIGoogle/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
+++ SystemUIGoogle/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
@@ -1,6 +1,7 @@
 package com.google.android.systemui.statusbar.phone;
 
 import android.widget.ImageView;
+import com.android.internal.util.du.Utils;
 import com.android.systemui.R;
 import com.android.systemui.Dependency;
 import com.android.systemui.dock.DockManager;
@@ -15,5 +16,16 @@ public class StatusBarGoogle extends StatusBar {
         DockObserver dockObserver = (DockObserver) Dependency.get(DockManager.class);
         dockObserver.setDreamlinerGear((ImageView) mStatusBarWindow.findViewById(R.id.dreamliner_gear));
         dockObserver.setIndicationController(new DockIndicationController(mContext));
+
+        // Enable MotionSense plugin
+        enableOsloPlugin();
+    }
+
+    private void enableOsloPlugin() {
+        if (Utils.isPackageInstalled(mContext, "com.google.oslo")) {
+            Utils.setComponentState(mContext, "com.google.oslo", "com.google.oslo.OsloOverlay", true);
+            Utils.setComponentState(mContext, "com.google.oslo", "com.google.oslo.OsloSensorManager", true);
+            Utils.setComponentState(mContext, "com.google.oslo", "com.google.oslo.service.OsloService", true);
+        }
     }
 }

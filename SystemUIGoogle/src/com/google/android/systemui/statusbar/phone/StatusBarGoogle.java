e1398efe90eb53a9775e827b1a7a9293785d2aba
Giuseppe Maggio <jertlok@protonmail.com>
2020-01-07 00:20:17 +0100

    Add StatusBarGoogle and increase integration
    Change-Id: I66013154ee03b560346726d780a8b9b1e2b7fb80

---
+++ SystemUIGoogle/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
@@ -0,0 +1,19 @@
+package com.google.android.systemui.statusbar.phone;
+
+import android.widget.ImageView;
+import com.android.systemui.R;
+import com.android.systemui.Dependency;
+import com.android.systemui.dock.DockManager;
+import com.android.systemui.statusbar.phone.StatusBar;
+import com.google.android.systemui.dreamliner.DockIndicationController;
+import com.google.android.systemui.dreamliner.DockObserver;
+
+public class StatusBarGoogle extends StatusBar {
+    @Override
+    public void start() {
+        super.start();
+        DockObserver dockObserver = (DockObserver) Dependency.get(DockManager.class);
+        dockObserver.setDreamlinerGear((ImageView) mStatusBarWindow.findViewById(R.id.dreamliner_gear));
+        dockObserver.setIndicationController(new DockIndicationController(mContext));
+    }
+}

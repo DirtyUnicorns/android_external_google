00a0b3759fe89d8c959f06e138fbea5c911f9301
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-02-04 16:27:06 -0500

    Smartspace: Import Lockscreen weather/live wallpaper [1/2]
    du: ported the code on top of our active edge code
    NB: this must be made here in aosp SystemUI to make it available for all
    devices, not only Soli/Squeeze ones.
    Our google sysui will inherit this code too when expanding aosp sysui classes.
    
    - Includes weather on lockscreen and cool stuff on ambient display
    - Import code from the fwb / adapt to use in external/Google by Surge1223 <Surge1223@gmail.com>
    
    Change-Id: I3b9062acc87a5e6161dc68b2d22bd988c0140cd7

--- SystemUIGoogle/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
+++ SystemUIGoogle/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
@@ -6,6 +6,9 @@ import com.android.systemui.R;
 import com.android.systemui.Dependency;
 import com.android.systemui.dock.DockManager;
 import com.android.systemui.statusbar.phone.StatusBar;
+import com.android.systemui.statusbar.NotificationLockscreenUserManager;
+import com.google.android.systemui.NotificationLockscreenUserManagerGoogle;
+import com.google.android.systemui.smartspace.SmartSpaceController;
 import com.google.android.systemui.dreamliner.DockIndicationController;
 import com.google.android.systemui.dreamliner.DockObserver;
 
@@ -19,6 +22,13 @@ public class StatusBarGoogle extends StatusBar {
 
         // Enable MotionSense plugin
         enableOsloPlugin();
+        ((NotificationLockscreenUserManagerGoogle) Dependency.get(NotificationLockscreenUserManager.class)).updateAodVisibilitySettings();
+    }
+
+    @Override
+    public void setLockscreenUser(int i) {
+        super.setLockscreenUser(i);
+        SmartSpaceController.get(this.mContext).reloadData();
     }
 
     private void enableOsloPlugin() {
